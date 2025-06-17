965050b975b72669b836145b2a967087def4b845
vinaychauhan01 <147112817+vinaychauhan01@users.noreply.github.com>
2025-06-17 22:23:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://fu-rin-hgn.onrender.com")
+URL = environ.get("URL", "https://fu-rin-hgn.onrender.com/")
 
 
 # Don't Remove Credit Tg - @VJ_Botz
